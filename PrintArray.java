4231f63f77ea81713871d411453f624080240294
HanniK <han.ni@viossolution.com>
2015-08-27 13:46:47 +0800

    Print array diagonally

---
+++ PrintArray.java
@@ -0,0 +1,40 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int arr[][] = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+        int rows = 5;
+        int cols = 5;
+        int orgCols = 4;
+        boolean reverse = false;
+
+        for (int lines = 1; lines < (rows + orgCols + 1); lines++) {
+            int num = 0;
+            if (reverse) {
+                if (orgCols > cols) {
+                    cols += 1;
+                }
+            }
+            if (!reverse) {
+                cols -= 1;
+                if (cols == 0) {
+                    reverse = true;
+                }
+            }
+            for (int j = 0; j <= orgCols; j++) {
+                int num2 = cols + j;
+                if (!reverse) {
+                    System.out.print("[" + (num) + "," + (num2) + "]");
+                } else {
+                    if (num2 < rows) {
+                        System.out.print("[" + (num2) + "," + (num) + "]");
+                    }
+                }
+                if ((num += 1) == lines) {
+                    break;
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
