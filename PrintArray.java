3cc48cf7848bd08468ad03c494b2bec1d1bdd75b
HanniK <han.ni@viossolution.com>
2015-08-27 13:50:32 +0800

    cleanup

--- PrintArray.java
+++ PrintArray.java
@@ -1,7 +1,6 @@
+
 public class PrintArray {
     public static void main(String[] args) {
-        int arr[][] = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-
         int rows = 5;
         int cols = 5;
         int orgCols = 4;
@@ -34,7 +33,6 @@ public class PrintArray {
                 }
             }
             System.out.println();
-
         }
     }
 }
